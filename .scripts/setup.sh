b875ec51f0b72840c62136d7ec1af55c5f3e5444
Jarvis Kenned <jarvis.k18@gmail.com>
2024-04-24 20:43:43 -0400

    update

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -3,9 +3,9 @@ sudo pacman -S --needed - < "$PWD/.scripts/pacman.txt"
 stow .
 
 
-eval "$PWD/ssh_setup"
-eval "$PWD/dotnet_install_script"
-eval "$PWD/install_git_repos"
+source "$PWD/ssh_setup"
+source "$PWD/dotnet_install_script"
+source "$PWD/install_git_repos"
 
 echo "Setting fish as the default shell"
 chsh -s $(which fish)
